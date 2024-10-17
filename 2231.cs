b9a22546f7679af388ae41a1e4a5faa1d68bee68
Lee Dahyun <cloud20211@naver.com>
2024-10-17 23:38:16 +0900

    solved.ac Lv2 3문제

---
+++ 2231.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 주어진 자연수 
+        int originNum = int.Parse(Console.ReadLine());
+        // 정답란
+        int answer = 0;
+
+        // 몇가지 수? n * 9
+        int origintmpN = originNum;
+        int howMany = 0;
+        while (origintmpN != 0)
+        {
+            origintmpN = origintmpN / 10;
+            howMany++;
+        }
+        howMany *= 9;
+
+        // 이 숫자부터 howMany만큼 검사하면 됨
+        int startNum = originNum - howMany;
+
+        for (int i = 0; i < howMany; i++)
+        {
+            // 각 자리 수 리스트
+            List<int> nums = new List<int>();
+
+            // 계산용 숫자. (startNum + i) : 현재 검사하는 수
+            int nowNum = (startNum + i);
+
+            // 각 자리 수 리스트에 추가하기
+            while (true)
+            {
+                nums.Add(nowNum % 10);
+                nowNum = nowNum / 10;
+                if (nowNum == 0) { break; }
+            }
+
+            // 각 자리 수 모두 더하기
+            for (int j = 0; j < nums.Count; j++)
+            {
+                nowNum += nums[j];
+            }
+
+            // 정답 맞나 체크
+            if (originNum == (startNum + i) + nowNum)
+            {
+                answer = startNum + i;
+                break;
+            }
+        }
+
+        Console.WriteLine(answer);
+    }
+}
---
+++ 30802.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int howMany = int.Parse(Console.ReadLine());
+        string[] shirtsSize = (Console.ReadLine().Split(' '));
+        // 티셔츠와 펜 묶음
+        string[] bundleN = (Console.ReadLine().Split(' '));
+        int totalShirts = 0;
+
+        for (int i = 0; i < shirtsSize.Length; i++)
+        {
+            int thisShirt = int.Parse(shirtsSize[i]);
+
+            if (thisShirt == 0) { continue; }
+
+            int tmp = thisShirt / int.Parse(bundleN[0]);
+            if (tmp > 0)
+            {
+                totalShirts += tmp;
+                if (thisShirt % int.Parse(bundleN[0]) > 0)
+                {
+                    totalShirts++;
+                }
+            }
+            else
+            {
+                totalShirts++;
+            }
+        }
+
+        // 티셔츠
+        Console.WriteLine("{0}", totalShirts);
+
+        // 펜
+        Console.WriteLine("{0} {1}",
+            (howMany / int.Parse(bundleN[1])),
+            (howMany % int.Parse(bundleN[1])));
+    }
+}
