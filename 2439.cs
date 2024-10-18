f425a2af19a9caf725644d90ec2d4e32047a7693
Lee Dahyun <cloud20211@naver.com>
2024-10-18 23:54:04 +0900

    solved.ac Lv1,Lv2 3문제

---
+++ 2439.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+
+        for (int many = 0; many < input; many++)
+        {
+            int tmp = input - many;
+            for (int i = 1; i < tmp; i++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int i = -1; i < many; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 2798.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 블랙잭
+
+        // 첫번쨰 입력 : 카드의 개수와 
+        string[] originNum = (Console.ReadLine()).Split(' ');
+
+        // 두번째 입력 : 카드에 쓰여 있는 수들
+        string[] strnums = (Console.ReadLine()).Split(' ');
+        int[] nums = new int[strnums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = int.Parse(strnums[i]);
+        }
+
+        // 최소 차이
+        int minDistance = 300000;
+        int answer = 0;
+
+        // 3차배열?
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i == j) continue;
+                for (int k = 0; k < nums.Length; k++)
+                {
+                    if (j == k || i == k) continue;
+                    // 셋의 합
+                    int thisNum = nums[i] + nums[j] + nums[k];
+                    int origin = int.Parse(originNum[1]);
+
+                    // 절대값이 최소보다 작은 수 + M을 넘지 않는 수
+                    if (origin - thisNum < minDistance && origin - thisNum >= 0)
+                    {
+                        minDistance = origin - thisNum;
+                        answer = thisNum;
+                    }
+
+                }
+            }
+        }
+
+        Console.WriteLine(answer);
+    }
+}
