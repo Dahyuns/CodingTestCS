56b2215503c7297685463e3a1ee2afe0ac0a8c2a
Lee Dahyun <cloud20211@naver.com>
2024-10-20 22:35:48 +0900

    solved.ac Lv1 1문제

---
+++ 10250.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int caseCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < caseCount; i++)
+        {
+            string[] caseN = Console.ReadLine().Split(' ');
+
+            // 총 층
+            int floor = int.Parse(caseN[0]);
+            // 총 방수
+            int room = int.Parse(caseN[1]);
+
+            // 몇번쨰 손님?
+            int custemer = int.Parse(caseN[2]);
+
+            // 몇 호?
+            int nowRoomN = 0;
+
+            for (int j = 0; j < room; j++)
+            {
+                for (int k = 0; k < floor; k++)
+                {
+                    // 방배정
+                    custemer--;
+                    if (custemer == 0)
+                    {
+                        nowRoomN = 100 * (k + 1) + (j + 1);
+                        break;
+                    }
+                }
+
+                if (custemer == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(nowRoomN);
+
+        }
+    }
+}
