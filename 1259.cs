01027b68476bdc392809abb62d0a3033070a9a7b
Lee Dahyun <cloud20211@naver.com>
2024-10-22 22:44:33 +0900

    solved.ac Lv2 1문제

---
+++ 1259.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string num = Console.ReadLine();
+
+            if (num == "0" || num == null) break;
+
+            while (num[0] == '0')
+            {
+                string tmp = "";
+                for (int i = 1; i < num.Length; i++)
+                {
+                    tmp += num[i];
+                }
+                num = tmp;
+            }
+
+            if (num == "0" || num == null) break;
+
+            bool isPelin = true;
+
+            // 자리수가 짝수일때
+            if (num.Length % 2 == 0)
+            {
+                for (int i = 0; i < num.Length / 2; i++)
+                {
+                    if (num[i] != num[num.Length - 1 - i])
+                    {
+                        isPelin = false;
+                        break;
+                    }
+                }
+            }
+            // 자리수가 홀수일때
+            else if (num.Length % 2 != 0)
+            {
+                for (int i = 0; i < (num.Length - 1) / 2; i++)
+                {
+                    if (num[i] != num[num.Length - 1 - i])
+                    {
+                        isPelin = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPelin)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
