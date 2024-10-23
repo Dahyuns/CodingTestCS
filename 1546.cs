6ae6a8a30b909f61c5118784da22bc44074ce43d
Lee Dahyun <cloud20211@naver.com>
2024-10-23 23:51:49 +0900

    solved.ac Lv2 1문제

---
+++ 1546.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float total = float.Parse(Console.ReadLine());
+        total = 0;
+
+        string[] scores = Console.ReadLine().Split(' ');
+
+        float best = 0;
+
+        // 최고점 구하기
+        for (int i = 0; i < scores.Length; i++)
+        {
+            int thisN = int.Parse(scores[i]);
+
+            if (best < thisN)
+            {
+                best = thisN;
+            }
+        }
+
+        // 새로운 점수들 합 구하기
+        for (int i = 0; i < scores.Length; i++)
+        {
+            float thisN = float.Parse(scores[i]);
+
+            total += thisN / best * 100;
+
+        }
+
+        Console.WriteLine(total / scores.Length);
+    }
+}
