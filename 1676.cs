bd61f6fe8a75d6c6882c6bb29fb9457dbfa3ce89
Lee Dahyun <cloud20211@naver.com>
2024-10-29 23:55:28 +0900

    solved.ac Lv2 1문제

---
+++ 1676.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int answer = 0;
+        int num = int.Parse(Console.ReadLine());
+
+        // 팩토리얼에서 5의 배수 개수 세기
+        for (int i = 5; i <= num; i *= 5)
+        {
+            answer += num / i;
+        }
+
+        Console.WriteLine(answer);
+
+    }
+}
